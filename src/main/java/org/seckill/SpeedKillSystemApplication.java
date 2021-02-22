af038d009feb5a2e6b745a7ee18a3b01185f6ea2
jayce <jayce404@foxmail.com>
2021-02-22 15:20:22 +0800

    第一次提交

---
+++ src/main/java/org/seckill/SpeedKillSystemApplication.java
@@ -0,0 +1,13 @@
+package org.seckill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeedKillSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeedKillSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/seckill/SpeedKillSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.seckill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeedKillSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
